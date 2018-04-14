0bb929fca85f9f6710475327d5ac1917abd01137
Nehal Patel <PR0Grammar@users.noreply.github.com>
2018-04-14 12:44:55 -0400

    Update floatToIntSum.asm

--- floatToIntSum.asm
+++ floatToIntSum.asm
@@ -1,28 +1,30 @@
 # Cross References:
-# f0: N	 
-# s3: sum
+# f0: N,	 
+# s3: Sum
 #####################################################################
 		.data
-prompt:		.asciiz	"\n   Please Input a value for N (N>=1) =  "
+prompt:		.asciiz	"\n\n Please Input a value for N =  "
+floatPrompt:	.asciiz "\n You Entered A FLOAT. Your number will be truncated."
 floatValue:     .asciiz "\n The float value entered: "
 intValue:	.asciiz "\n The truncated integer used: "
-floatOneConst:	.float	0.9999999
+floatOneConst:	.float 0.9999999 # This value allows for '1 - 1.xxxxxxx' to be entered
 result:		.asciiz	"\n The sum of the integers from 1 to N is "
 bye:		.asciiz	"\n  **** Adios Amigo - Have a good day ****"
 		.globl	main
 		.text
 main:		
 		#Ask user for value (float value)
-		li	$v0, 4		
+		li	$v0, 4		# system call code for Print String
 		la	$a0, prompt 	# load address of prompt into $a0
 		syscall			# print the prompt message
 
 		#Read the value and stored in ***$f0***
 		li	$v0, 6		# system call code for Read Float
 		syscall			# reads the value of N into $f0
+
 		
-		#Terminate if N <=1
-		l.s	$f30, floatOneConst
+		#Terminate if N <=0
+		l.s	$f30, floatOneConst #load const into $f30
 		c.le.s	1, $f0, $f30	#if user input less than 1
 		bc1t	1, end	#goto end
 		li	$v0, 0	# clear register $v0 to zero
@@ -43,13 +45,6 @@ removeExp:
 		
 		#Add implied bit
 		addi $t3, $t3, 8388608
-		
-getFrac:	
-		#Gets fractional portion
-		sllv $t4, $t3, $t2
-		sll $t4, $t4, 9
-		
-		move $s1, $t4 # **** $s1 contains fractional portion(if this value equals zero, the input was an integer)
 
 getInt:
 		#Gets the integer porition as the first bits 
@@ -64,9 +59,25 @@ getInt:
 		srlv $t5, $t5, $s0
 		
 		move $s2, $t5 #**** $s2 contains integer portion
+		
+		
+getFrac:	
+		#Gets fractional portion
+		sllv $t4, $t3, $t2
+		sll $t4, $t4, 9
+		
+		move $s1, $t4 # **** $s1 determines if value entered is float or int (int if $s1 == 0)
+		beqz $s1, loop #If value entered is an int, skip the truncation prompt
 
-printFloatInt:
-		#Prints the float value and integer value
+printFloatEntered:
+
+
+		#Alert user that value entered is float
+		li	$v0, 4 
+		la	$a0, floatPrompt
+		syscall
+		
+		#Alert user the float value
 		li $v0, 4
 		la $a0, floatValue
 		syscall
@@ -75,6 +86,7 @@ printFloatInt:
 		mov.s $f12, $f0
 		syscall
 		
+		#Alert user the truncated int
 		li $v0, 4
 		la $a0, intValue
 		syscall
@@ -84,11 +96,11 @@ printFloatInt:
 		syscall
 	
 loop:		
-		add	$s3, $s3, $t5		# sum of integers in register $s2
+		add	$s3, $s3, $t5		# sum of integers in register $s2 ($t5 also contains same value)
 		addi	$t5, $t5, -1		# summing integers in reverse order
 		bnez	$t5,  loop		# branch to loop if $v0 is != zero		
-		li	$v0, 4		
-		la	$a0, result		# load address of message into $a0
+		li	$v0, 4		# system call code for Print String
+		la	$a0, result	# load address of message into $a0
 		syscall			# print the string
 	
 		li	$v0, 1		# system call code for Print Integer
@@ -96,8 +108,9 @@ loop:
 		syscall			# print sum of integers
 		li $s3, 0
 		b 	main		# branch to main
+		
 
-end:		li	$v0, 4		
+end:		li	$v0, 4		# system call code for Print String
 		la	$a0, bye	# load address of msg. into $a0
 		syscall			# print the string
 
